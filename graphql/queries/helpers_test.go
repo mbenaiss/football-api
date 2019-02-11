18effab301ac00501c4e239692085cbff3920650
Mahmoud BEN AISSA <mbenaissa@vente-privee.com>
2019-02-11 18:00:18 +0100

    update test

--- graphql/queries/helpers_test.go
+++ graphql/queries/helpers_test.go
@@ -1,6 +1,7 @@
 package queries
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/go-graphql-football-api/models"
@@ -30,25 +31,60 @@ func TestFilters(t *testing.T) {
 		},
 	}
 
-	results := filter(matches, map[string]interface{}{
-		"competition": "Primera Division",
-	})
-
-	if len(results) < 2 {
-		t.Errorf("expected 2 , got %+v", results)
+	testScenarios := []struct {
+		given, expected []models.MatchResult
+		f               map[string]interface{}
+		expectedError   bool
+	}{
+		{
+			given: matches,
+			expected: []models.MatchResult{
+				models.MatchResult{
+					Competition: "Primera Division",
+					Status:      "FINISHED",
+				},
+				models.MatchResult{
+					Competition: "Primera Division",
+					Status:      "SCHEDULED",
+				}},
+			expectedError: false,
+			f: map[string]interface{}{
+				"competition": "Primera Division",
+			},
+		},
+		{
+			f: map[string]interface{}{
+				"competition": "Primera Division",
+				"status":      "SCHEDULED",
+			},
+			expected: []models.MatchResult{
+				models.MatchResult{
+					Competition: "Primera Division",
+					Status:      "SCHEDULED",
+				},
+			},
+			expectedError: false,
+		},
+		{
+			given:         matches,
+			expected:      matches,
+			f:             map[string]interface{}{},
+			expectedError: false,
+		},
+		{
+			given:         matches,
+			expected:      matches,
+			f:             map[string]interface{}{},
+			expectedError: true,
+		},
 	}
 
-	results = filter(matches, map[string]interface{}{
-		"competition": "Primera Division",
-		"status":      "SCHEDULED",
-	})
-
-	if len(results) != 1 {
-		t.Errorf("expected 1 , got len(%+v) %+v", len(results), results)
-	}
-
-	results = filter(matches, map[string]interface{}{})
-	if len(results) != 5 {
-		t.Errorf("expected 0 , got len(%+v) %+v", len(results), results)
+	for i, s := range testScenarios {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			results := filter(matches, s.f)
+			if len(s.expected) != len(results) && !s.expectedError {
+				t.Errorf("expected len : %d \n %+v \ngot len : %d %+v", len(s.expected), s.expected, len(results), results)
+			}
+		})
 	}
 }
