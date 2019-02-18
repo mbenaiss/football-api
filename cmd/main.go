0a6ee589d39216cda3833f209934edf395cc9f9d
Mahmoud BEN AISSA <mbenaissa@vente-privee.com>
2019-02-18 13:33:07 +0100

    fix gcp

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,10 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/mbenaiss/football-api/function"
+	"github.com/mbenaiss/football-api/gcp"
 )
 
 func main() {
-	http.HandleFunc("/graphql", function.Matches)
+	http.HandleFunc("/graphql", gcp.Matches)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- makefile
+++ makefile
@@ -1,10 +1,7 @@
-.PHONY: dependecy clean deploy dev
-
-dependecy: clean
-	env GO111MODULE=on go mod init  && env GO111MODULE=on go mod tidy
+.PHONY: dev
 
 dev:
 	go run cmd/main.go
 
-clean:
-	rm -rf ./vendor ./go.mod ./go.sum
+deploy-gcp:
+	cd gcp && make deploy
